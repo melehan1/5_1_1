728d360313f79b29c6debd29f01bc4b174ca70f0
Дмитрий <melehan@mail.ru>
2024-06-26 11:43:49 +0300

    Add files via upload

---
+++ module_5_1_1.py
@@ -0,0 +1,12 @@
+class House:
+    def __init__(self, numberOfFloors):
+        self.numberOfFloors = numberOfFloors
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
+        print(self.numberOfFloors)
+
+
+h1 = House(0)
+h1.setNewNumberOfFloors(9)
